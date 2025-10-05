29462d38d70ddcfab7930f25193d422d940790eb
IMRAN-Anisha <anisha.imran@outlook.com>
2025-10-05 22:15:20 +0800

    Refresh App

---
+++ React Website /my-wow-site/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My Wow Website</h1>
+      <p>This is going to be amazing!</p>
+    </div>
+  );
+}
+
+export default App;
