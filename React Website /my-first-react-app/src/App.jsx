cf3ebacf59b9dcfb5bc2f654a3c06ffff6035dd3
IMRAN-Anisha <anisha.imran@outlook.com>
2025-10-08 13:06:31 +0800

    Search Bar

--- React Website /my-first-react-app/src/App.jsx
+++ React Website /my-first-react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Search from "./components/search";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
   return (
     <main>
-      <div> className = "pattern" </div>
-      <div> className = "wrapper" </div>
-      <header><span className = "text-gradient" >Find Movies You'll Enjoy </span></header>
-      <p>Search</p>
+      <div className = "pattern" ></div>
+      <div className = "wrapper" >
+      <header>
+        <img src = "./hero.png" alt = "Hero Banner"></img>
+        <h1><span className = "text-gradient" >Find Movies You'll Enjoy </span></h1>
+      </header>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
     </main>
   )
 }
